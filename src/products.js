a399f2ecea0a7eadae9d5c62f8c187644fa68880
himanshi-wadhwa-21 <162698068+himanshi-wadhwa-21@users.noreply.github.com>
2024-05-09 21:06:04 +0530

    almost final build

--- src/products.js
+++ src/products.js
@@ -6,41 +6,40 @@ import product5 from "./assets/pedroimg (5).png";
 import product6 from "./assets/pedroimg (6).png";
 
 export const PRODUCTS = [
-{
-id: 1,
-productName: "IPhone10",
-price: 99000,
-productImage: product1,
-},
- {
+  {
+    id: 1,
+    productName: "IPhone10",
+    price: 99000,
+    productImage: product1,
+  },
+  {
     id: 2,
     productName: "Star Cap",
     price: 970,
     productImage: product2,
- },
- {
+  },
+  {
     id: 3,
     productName: "Flower cap",
     price: 850,
     productImage: product3,
- },
- {
+  },
+  {
     id: 4,
     productName: "Black Rose tee",
     price: 1259,
     productImage: product4,
- },
- {
+  },
+  {
     id: 5,
     productName: "Escapism tee",
     price: 1459,
     productImage: product5,
- },
- {
+  },
+  {
     id: 6,
     productName: "Oversized Denim",
     price: 3200,
     productImage: product6,
- },
-    
+  },
 ];
